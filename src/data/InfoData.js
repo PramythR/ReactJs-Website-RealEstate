81be38e27577ea2a7f458caa91990925b70c852d
PramithR <PramithRajapaksha69@gmail.com>
2021-04-29 15:34:21 +0530

    my

---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from "../images/int1.jpg";
+import ImageTwo from "../images/int2.jpg";
+
+export const InfoData = {
+  heading: "Explore our Beautiful Homes",
+  paragraphOne:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  paragraphTwo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+  buttonLabel: "View Home",
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+export const InfoDataTwo = {
+  heading: "Modern Designs",
+  paragraphOne: "lorem",
+  paragraphTwo: "Lorem twos",
+  buttonLabel: "View Home",
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
---
+++ src/data/SliderData.js
@@ -0,0 +1,73 @@
+import ImageOne from "../images/house1.jpg";
+import ImageTwo from "../images/house2.jpg";
+import ImageThree from "../images/house3.jpg";
+import ImageFour from "../images/house_1.jpg";
+
+export const SliderData = [
+  {
+    title: "Luxury  villa in Bali Indonesia",
+    price: "$4,200,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "Boquete, Panama Modern Villa",
+    price: "$4,500,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageTwo,
+    alt: "House",
+  },
+  {
+    title: "Kaliningrad, Калининградская область, Россия",
+    price: "$8,750,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageThree,
+    alt: "House",
+  },
+  {
+    title: "Guardamar del Segura, Spain",
+    price: "5,200,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageFour,
+    alt: "House",
+  },
+];
+// export const SliderDataTwo = [
+//   {
+//     title: "Luxury  villa in Bali Indonesia",
+//     price: "$4,200,000",
+//     path: "/homes",
+//     label: "View Home",
+//     image: ImageOne,
+//     alt: "House",
+//   },
+//   {
+//     title: "Luxury  villa in Bali Indonesia",
+//     price: "$4,200,000",
+//     path: "/homes",
+//     label: "View Home",
+//     image: ImageOne,
+//     alt: "House",
+//   },
+//   {
+//     title: "Luxury  villa in Bali Indonesia",
+//     price: "$4,200,000",
+//     path: "/homes",
+//     label: "View Home",
+//     image: ImageOne,
+//     alt: "House",
+//   },
+//   {
+//     title: "Luxury  villa in Bali Indonesia",
+//     price: "$4,200,000",
+//     path: "/homes",
+//     label: "View Home",
+//     image: ImageOne,
+//     alt: "House",
+//   },
+// ];
